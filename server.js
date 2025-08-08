20e339b7dda937138b5046b7cf23816ed37ba311
Shrutirawat27 <shrutirawat0208@gmail.com>
2025-08-08 18:03:45 +0530

    Updated backend: replaced localhost URLs with env variables

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ mongoose
   .then(() => {
     console.log('MongoDB connected');
     app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => console.error('MongoDB connection error:', err));
